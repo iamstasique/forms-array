f0284c76d6f69d7e412fb5b98ed8a7f9b4c1efc2
Stanislau <stanislau.ihnatsenka.qrder@gmail.com>
2024-06-20 11:41:41 +0200

    create forms-list and form-card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {RouterOutlet} from "@angular/router";
-import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
-import {MockBackendInterceptor} from "./shared/mock-backend/mock-backend.interceptor";
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterOutlet } from "@angular/router";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { FormsListComponent } from './pages/forms-list/forms-list.component';
+import { MockBackendInterceptor } from "./shared/mock-backend/mock-backend.interceptor";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterOutlet,
-    NgbModule
+    NgbModule,
+    FormsListComponent
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
@@ -19,4 +21,4 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/pages/forms-list/forms-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms-list.component.html',
+  styleUrl: './forms-list.component.scss'
+})
+export class FormsListComponent {
+}
