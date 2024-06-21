f5d10504a17656d0bd0496508ba0a495dbb0c0e3
Stanislau <stanislau.ihnatsenka.qrder@gmail.com>
2024-06-21 13:41:09 +0200

    refactor mock-backend interceptor

--- src/app/shared/mock-backend/mock-backend.interceptor.ts
+++ src/app/shared/mock-backend/mock-backend.interceptor.ts
@@ -5,15 +5,13 @@ import {
   HttpRequest,
   HttpResponse
 } from '@angular/common/http';
-import {Injectable} from "@angular/core";
-import {delay, Observable, of, tap} from "rxjs";
-import {Country} from "../enum/country";
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { Country } from "../enum/country";
 
 @Injectable()
 export class MockBackendInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-   console.log(`Intercepted request: ${req.url}`);
-
     const countryRegion = {
       [Country.Australia]: 'Australia',
       [Country.Poland]: 'Europe',
@@ -26,17 +24,17 @@ export class MockBackendInterceptor implements HttpInterceptor {
 
     // Intercept requests and return mock responses
     if (req.url.endsWith('/api/regions') && req.method === 'POST') {
-      const {country}: {country: Country} = req.body;
-      return of(new HttpResponse({ status: 200, body: { region: countryRegion[country]} }));
+      const { country }: { country: Country } = req.body;
+      return of(new HttpResponse({ status: 200, body: { region: countryRegion[country] } }));
     }
     if (req.url.endsWith('/api/checkUsername') && req.method === 'POST') {
-      const {username} = req.body;
+      const { username } = req.body;
       return of(new HttpResponse({ status: 200, body: { isAvailable: username.includes('new') } }));
     }
     if (req.url.endsWith('/api/submitForm') && req.method === 'POST') {
       return of(new HttpResponse({ status: 200, body: { result: 'nice job' } }));
     }
 
-    return of(new HttpResponse({ status: 404, body: { result: 'You are using the wrong endpoint'} }));
+    return of(new HttpResponse({ status: 404, body: { result: 'You are using the wrong endpoint' } }));
   }
 }
