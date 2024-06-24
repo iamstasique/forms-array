e7bd0501fe7ac4fb1256a79bd103103a0239b898
Stanislau <stanislau.ihnatsenka.qrder@gmail.com>
2024-06-24 11:58:56 +0200

    make 'submit forms' button text and tooltip text dynamic

--- src/app/components/submit-forms-button/submit-forms-button.component.ts
+++ src/app/components/submit-forms-button/submit-forms-button.component.ts
@@ -1,18 +1,19 @@
 import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, DestroyRef, EventEmitter, Input, Output, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
-import { BehaviorSubject, Observable, Subject, map, takeUntil, timer } from 'rxjs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BehaviorSubject, Observable, Subject, map, startWith, takeUntil, timer } from 'rxjs';
 import { FormsService } from '../../services/forms.service';
 
 @Component({
   selector: 'app-submit-forms-button',
   standalone: true,
-  imports: [AsyncPipe, MatButtonModule],
+  imports: [AsyncPipe, MatButtonModule, MatTooltipModule],
   templateUrl: './submit-forms-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SubmitFormsButtonComponent {
+export class SubmitFormsButtonComponent implements OnInit {
   @Input({ required: true }) disabled: boolean = false;
   @Output() onSubmit: EventEmitter<void> = new EventEmitter<void>();
 
@@ -21,10 +22,25 @@ export class SubmitFormsButtonComponent {
   isTimerActive: boolean = false;
   secondsLeftSubject: BehaviorSubject<number> = new BehaviorSubject<number>(this.initialTimerValue);
 
+  submitText$!: Observable<string>;
+  tooltipText$!: Observable<string>;
+
   private timer$!: Observable<number>;
   private cancelTimerSubject: Subject<void> = new Subject<void>();
   private destroyRef: DestroyRef = inject(DestroyRef);
   private formsService: FormsService = inject(FormsService);
+
+  ngOnInit(): void {
+    this.submitText$ = this.formsService.formArray.valueChanges.pipe(
+      startWith(this.formsService.formArray),
+      map(formArray => `Submit ${formArray.length > 1 ? 'all forms' : 'form'}`)
+    )
+
+    this.tooltipText$ = this.formsService.formArray.valueChanges.pipe(
+      startWith(this.formsService.formArray),
+      map(formArray => `Your ${formArray.length > 1 ? 'forms are' : 'form is'} invalid`)
+    )
+  }
 
   getResultTimerValue(seconds: number): string {
     return `0:${seconds < 10 ? '0' + seconds : seconds}`;
